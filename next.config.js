7ac538f36a18d7e4fdb5e298b042146e9d19adbb
pjh9898 <55084024+pjh9898@users.noreply.github.com>
2025-08-14 15:19:40 +0900

    vercel배포 설정중

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@ const createNextIntlPlugin = require("next-intl/plugin");
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    const path = require("path");
+
+    // @ 경로를 src로 매핑
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+
+    return config;
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
